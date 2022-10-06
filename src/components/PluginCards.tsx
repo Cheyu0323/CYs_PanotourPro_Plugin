91bd535727fb67dd878fc22f38afcf599dacf285
Cheyu0323 <zxzx44585@gmail.com>
2022-10-06 20:34:58 +0800

    fix responsive layout

--- src/components/PluginCards.tsx
+++ src/components/PluginCards.tsx
@@ -1,10 +1,14 @@
-import { Box, Grid, } from "@mui/material";
+import { Box, Grid } from "@mui/material";
 import Card from "../components/Card";
 import pluginInfo from "../data/plugininfo";
 
 const PluginCards = () => {
     return (
-        <Box width="100%" sx={{ maxWidth: "lg" }} paddingBottom={{sm:"0", xs: "5%"}}>
+        <Box
+            width="100%"
+            sx={{ maxWidth: "lg" }}
+            paddingBottom={{ sm: "0", xs: "5%" }}
+        >
             <Grid container spacing={2}>
                 {pluginInfo.map((item) => {
                     let Icon = item.icon;
